5d426f3d787eb66b4eed11292aee2afca2e751eb
ewaldherr <waldherr49@gmail.com>
2024-12-02 11:31:15 +0100

    actually remove entries from vector

--- day2/main.cpp
+++ day2/main.cpp
@@ -25,28 +25,44 @@ void readGridToVector(const std::string& filename, std::vector<std::vector<int>>
     file.close();
 }
 
-int checkDecreasingLine(std::vector<int>& vec){
-    int errors = 0;
+int checkDecreasingLine(std::vector<int>& vec, int errors){
     for(int i = 0; i + 1 < vec.size(); ++i){
         int diff = vec[i] - vec[i+1];
         if(diff > 3 || diff < 1){
-            errors++;
-            if(errors==2){
+            if(errors==1){
                 return 0;
+            } else{
+                std::vector<int> copy = vec;
+                copy.erase(i);
+                vec.erase(i+1);
+                int cascade = checkDecreasingLine(vec,1) + checkDecreasingLine(copy,1);
+                if(cascade > 0){
+                    return 1;
+                } else{
+                    return 0;
+                }
             }
         }
     }
     return 1;
 }
 
-int checkIncreasingLine(std::vector<int>& vec){
-    int errors = 0;
+int checkIncreasingLine(std::vector<int>& vec, int errors){
     for(int i = 0; i + 1 < vec.size(); ++i){
         int diff = vec[i+1] - vec[i];
         if(diff > 3 || diff < 1){
-            errors++;
             if(errors==2){
                 return 0;
+            } else{
+                std::vector<int> copy = vec;
+                copy.erase(i);
+                vec.erase(i+1);
+                int cascade = checkDecreasingLine(vec,1) + checkDecreasingLine(copy,1);
+                if(cascade > 0){
+                    return 1;
+                } else{
+                    return 0;
+                }
             }
         }
     }
@@ -56,8 +72,8 @@ int checkIncreasingLine(std::vector<int>& vec){
 int GetValidLines(std::vector<std::vector<int>>& grid){
     int sum = 0;
     for(auto vec: grid){
-        sum += checkIncreasingLine(vec);
-        sum += checkDecreasingLine(vec);
+        sum += checkIncreasingLine(vec, 0);
+        sum += checkDecreasingLine(vec, 0);
     }
     return sum;
 }
