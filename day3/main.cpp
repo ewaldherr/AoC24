105ad0a0e9016804ec84a59f73edc7c442a4c0b2
ewaldherr <waldherr49@gmail.com>
2024-12-03 07:04:47 +0100

    add isdigit fct

--- day3/main.cpp
+++ day3/main.cpp
@@ -25,6 +25,11 @@ void readGridToVector(const std::string& filename, std::vector<std::vector<char>
     file.close();
 }
 
+bool isDigit(char c){
+    int i = c - '0';
+    return (i > -1 && i < 10);
+}
+
 int multiply(int a1, int a2, int a3, int b1, int b2, int b3){
     return a1*100*b1*100+a1*100*b2*10+a1*100*b3+a2*10*b1*100+a2*10*b2*10+a2*10*b3+a3*b1*100+a3*b2*10+a3*b3;
 }
@@ -35,7 +40,7 @@ int checkPosition(std::vector<char> & vec, int pos){
         if(!isDigit(vec[pos+3])){
             return 0;
         } else {
-            a1 = std::atoi(vec[pos+3]);
+            a1 = vec[pos+3] - '0';
         }
         if(!isDigit(vec[pos+4])){
             if(vec[pos+4] != ','){
@@ -45,7 +50,7 @@ int checkPosition(std::vector<char> & vec, int pos){
             if(!isDigit(vec[pos+5])){
             return 0;
             } else {
-                b1 = std::atoi(vec[pos+5]);
+                b1 = vec[pos+5] - '0';;
             }
             if(!isDigit(vec[pos+6])){
                 if(vec[pos+6] != ')'){
@@ -54,7 +59,7 @@ int checkPosition(std::vector<char> & vec, int pos){
                 //second number is 1 digit long 
                 return multiply(0,0,a1,0,0,b1);
             } else{
-                a2 = std::atoi(vec[pos+6]);
+                a2 = vec[pos+6] - '0';;
             }
             if(!isDigit(vec[pos+7])){
                 if(vec[pos+7] != ')'){
@@ -70,7 +75,7 @@ int checkPosition(std::vector<char> & vec, int pos){
             }
             return multiply(0,0,a1,b1,b2,b3);
         } else{
-            a2 = std::atoi(vec[pos+4]);
+            a2 = vec[pos+4] - '0';
         }
         if(!isDigit(vec[pos+5])){
             if(vec[pos+5] != ','){
@@ -80,7 +85,7 @@ int checkPosition(std::vector<char> & vec, int pos){
             if(!isDigit(vec[pos+6])){
             return 0;
             } else {
-                b1 = std::atoi(vec[pos+6]);
+                b1 = vec[pos+6] - '0';
             }
             if(!isDigit(vec[pos+7])){
                 if(vec[pos+7] != ')'){
@@ -89,7 +94,7 @@ int checkPosition(std::vector<char> & vec, int pos){
                 //second number is 1 digit long 
                 return multiply(0,a1,a2,0,0,b1);
             } else{
-                a2 = std::atoi(vec[pos+7]);
+                a2 = vec[pos+7] - '0';
             }
             if(!isDigit(vec[pos+8])){
                 if(vec[pos+8] != ')'){
@@ -98,14 +103,14 @@ int checkPosition(std::vector<char> & vec, int pos){
                 //second number is 2 digits long
                 return multiply(0,a1,a2,0,b1,b2);
             } else{
-                b3 = std::atoi(vec[pos+8]);
+                b3 = vec[pos+8] - '0';
             } 
             if(vec[pos+9]!= ')'){
                 return 0;
             }
             return multiply(0,a1,a2,b1,b2,b3);
         } else{
-            a3 = std::atoi(vec[pos+5]);
+            a3 = vec[pos+5] - '0';
         }
         if(vec[pos+6]!= ','){
             return 0;
@@ -114,7 +119,7 @@ int checkPosition(std::vector<char> & vec, int pos){
         if(!isDigit(vec[pos+7])){
             return 0;
         } else {
-            b1 = std::atoi(vec[pos+7]);
+            b1 = vec[pos+7] - '0';
         }
         if(!isDigit(vec[pos+4])){
             if(vec[pos+4] != ')'){
@@ -123,7 +128,7 @@ int checkPosition(std::vector<char> & vec, int pos){
             //second number is 1 digit long 
             return multiply(a1,a2,a3,0,0,b1);
         } else{
-            a2 = std::atoi(vec[pos+8]);
+            b2 = vec[pos+8] - '0';
         }
         if(!isDigit(vec[pos+5])){
             if(vec[pos+5] != ')'){
@@ -132,7 +137,7 @@ int checkPosition(std::vector<char> & vec, int pos){
             //second number is 2 digits long
             return multiply(a1,a2,a3,0,b1,b2);
         } else{
-            b3 = std::atoi(vec[pos+9]);
+            b3 = vec[pos+9] - '0';
         } 
         if(vec[pos+10]!= ')'){
             return 0;
