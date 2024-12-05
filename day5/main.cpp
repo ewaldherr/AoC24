9039ef650e74412fc0226b008c35731b08ec02b4
ewaldherr <waldherr49@gmail.com>
2024-12-05 07:19:19 +0100

    initial solution part 2

--- day5/main.cpp
+++ day5/main.cpp
@@ -64,7 +64,6 @@ int checkLine(std::vector<int>& vec, std::multimap<int,int>& rules){
             for(start;start!=end;++start){
                 auto [k,v] = *start;
                 if(v == vec[j]){
-                    std::cout << v << " needs to be in front of " << vec[i] << std::endl;
                     return 0;
                 }
             }
@@ -73,17 +72,48 @@ int checkLine(std::vector<int>& vec, std::multimap<int,int>& rules){
     return vec[vec.size()/2];
 }
 
-int checkGrid(std::vector<std::vector<int>>& grid, std::multimap<int,int>& rules){
+int correctLine(std::vector<int>& vec, std::multimap<int,int>& rules){
+    for(int i = 0; i < vec.size(); ++i){
+        for(int j = i + 1; j < vec.size(); j++){
+            auto range = rules.equal_range(vec[i]);
+            auto start = range.first;
+            auto end = range.second;
+            for(start;start!=end;++start){
+                auto [k,v] = *start;
+                if(v == vec[j]){
+                    std::swap(vec[i],vec[j]);
+                }
+            }
+        }
+    }
+    return vec[vec.size()/2];
+}
+
+int correctLines(std::multimap<int,int>& rules, std::vector<std::vector<int>>& incorrectLines){
+    int sum = 0;
+    for(auto vec: incorrectLines){
+        sum += correctLine(vec, rules);
+    }
+    return sum;
+}
+
+int checkGrid(std::vector<std::vector<int>>& grid, std::multimap<int,int>& rules, std::vector<std::vector<int>>& incorrectLines){
     int sum = 0;
     for(auto vec: grid){
-        sum += checkLine(vec, rules);
+        int current = checkLine(vec, rules);
+        if(current == 0){
+            incorrectLines.push_back(vec);
+        }
+        sum += current;
     }
     return sum;
 }
 
 int main(int argc, char** argv){
     std::vector<std::vector<int>> grid;
+    std::vector<std::vector<int>> incorrectLines;
     std::multimap<int,int> rules;
     readSections(argv[1], grid, rules);
-    std::cout << "The result is " << checkGrid(grid, rules) << std::endl;
+    std::cout << "The result for part 1 is " << checkGrid(grid, rules, incorrectLines) << std::endl;
+    std::cout << "The result for part 1 is " << correctLines(rules, incorrectLines) << std::endl;
 }
